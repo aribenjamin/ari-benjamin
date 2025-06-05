9e7e1e607dd020fb21f71262b5803b0314a3166f
aribenjamin <arisbenjamin@gmail.com>
2025-06-05 15:56:20 +0000

    Deploying to gh-pages from @ aribenjamin/ari-benjamin@502f63701c1a8797b0ffbec68f185fc7df3249e2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -171,13 +171,6 @@ ninja.data = [{
           window.open("https://github.com/aribenjamin", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
