75911a41e3cdd557af104ecb41fcf5388a15dc1a
aribenjamin <arisbenjamin@gmail.com>
2025-06-05 15:09:28 +0000

    Deploying to gh-pages from @ aribenjamin/ari-benjamin@ada19ab85e38aa724e7dc4a6865b8cb2dc92a6ed 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,17 +25,10 @@ ninja.data = [{
           },
         },{id: "nav-repositories",
           title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
-          },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
           },
         },{id: "dropdown-bookshelf",
               title: "bookshelf",
@@ -51,7 +44,62 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/blog/";
               },
-            },{id: "books-the-godfather",
+            },{id: "post-why-do-we-love-a-sauna",
+        
+          title: "Why do we love a sauna?",
+        
+        description: "Towards a computational theory of embodied emotion.",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2023/embodied-emotion/";
+          
+        },
+      },{id: "post-where-is-wonder-in-computational-neuroscience",
+        
+          title: "Where is wonder in computational neuroscience?",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2021/wonder/";
+          
+        },
+      },{id: "post-hidden-perils-of-a-learning-pill",
+        
+          title: "Hidden perils of a learning pill",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2021/learning-pill/";
+          
+        },
+      },{id: "post-visualizing-my-research-interests",
+        
+          title: "Visualizing my research interests",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2021/interests/";
+          
+        },
+      },{id: "post-redesigning-the-kording-lab-39-s-practices",
+        
+          title: "Redesigning the Kording Lab&#39;s practices",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2019/lab-redesign/";
+          
+        },
+      },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
           section: "Books",handler: () => {
@@ -102,11 +150,25 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
             },},{
+        id: 'social-bluesky',
+        title: 'Bluesky',
+        section: 'Socials',
+        handler: () => {
+          window.open("aribenjamin.bsky.social", "_blank");
+        },
+      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%61%72%69%73%62%65%6E%6A%61%6D%69%6E@%67%6D%61%69%6C.%63%6F%6D", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/aribenjamin", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -116,25 +178,11 @@ ninja.data = [{
           window.open("https://inspirehep.net/authors/1010907", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=GW6D4ZIAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
